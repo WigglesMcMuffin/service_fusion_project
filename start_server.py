0e51439b20f06c5e2f7b172ad18687724c32ed1a
Tipene Moss <teee.moose@gmail.com>
2016-08-13 21:57:46 -0400

    Put env variable in the correct location

--- start_server.py
+++ start_server.py
@@ -2,5 +2,5 @@ from flask_app import create_app
 import os
 
 if __name__ == '__main__':
-  app = create_app()
-  app.run(host='0.0.0.0', port=int(os.environ['PORT']),  debug=True, env='prod')
+  app = create_app(env='prod')
+  app.run(host='0.0.0.0', port=int(os.environ['PORT']),  debug=True)
